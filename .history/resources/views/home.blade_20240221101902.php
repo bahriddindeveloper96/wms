34757bf18d65a8750c788ef3c0d993ec30e4c000
Bahriddin <bahriddin.developer962gmail.com>
2024-02-21 10:47:48 +0500

    (one to one)

---
+++ .history/resources/views/home.blade_20240221101902.php
@@ -0,0 +1,3 @@
+@extends('adminlte::page')
+@section('title','Dashboard')
+@section('content_header')
---
+++ .history/resources/views/home.blade_20240221102108.php
@@ -0,0 +1,10 @@
+@extends('adminlte::page')
+@section('title','Dashboard')
+@section('content_header')
+<h1>Dashboard</h1>
+@stop
+@section('content')
+<p>Welcome to Uzbekistan</p>
+@stop
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
